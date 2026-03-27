953136cf93e8835a0e0671e55f5543f6b4cfbed2
joveem <joveem@jovdev.com>
2026-03-27 11:04:57 -0300

    [ FAKE-STASH ]

--- localization-service/default-localization.config.ts
+++ localization-service/default-localization.config.ts
@@ -75,10 +75,10 @@ const RAW_TERMS = [
         },
     },
     {
-        TermKey: 'home-where-and-when-will-BASE-ANGULAR-PROJECT-01-pass',
+        TermKey: 'home-where-and-when-will-jd-eh-ws-fe-01-pass',
         TermValueByLanguage: {
-            'pt-br': 'Por onde a $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 vai passar e quando',
-            'en-us': 'Where and when will $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 be available',
+            'pt-br': 'Por onde a $Env Heaven-01 vai passar e quando',
+            'en-us': 'Where and when will $Env Heaven-01 be available',
         },
     },
     {
@@ -148,9 +148,9 @@ const RAW_TERMS = [
         TermKey: 'home-blackjack-21-against-players-or-table',
         TermValueByLanguage: {
             'pt-br':
-                'Blackjack (21) contra outros jogadores ou contra a mesa. Os jogadores poderão jogar sem ter que deixar a $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 depositada em qualquer outro lugar que não seja a própria wallet.',
+                'Blackjack (21) contra outros jogadores ou contra a mesa. Os jogadores poderão jogar sem ter que deixar a $Env Heaven-01 depositada em qualquer outro lugar que não seja a própria wallet.',
             'en-us':
-                'Blackjack (21) against other players or the table. Players will be able to play without having to leave their $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 deposited anywhere other than their own wallet.',
+                'Blackjack (21) against other players or the table. Players will be able to play without having to leave their $Env Heaven-01 deposited anywhere other than their own wallet.',
         },
     },
     {
@@ -187,9 +187,9 @@ const RAW_TERMS = [
         TermKey: 'home-poker-texas-holdem-against-players',
         TermValueByLanguage: {
             'pt-br':
-                "Poker (Texas Hold'em) contra outros jogadores. Os jogadores poderão jogar sem ter que deixar a $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 depositada em qualquer outro lugar que não seja a própria wallet.",
+                "Poker (Texas Hold'em) contra outros jogadores. Os jogadores poderão jogar sem ter que deixar a $Env Heaven-01 depositada em qualquer outro lugar que não seja a própria wallet.",
             'en-us':
-                "Poker (Texas Hold’em) against other players. Players will be able to play without having to leave their $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 deposited anywhere other than their own wallet.",
+                "Poker (Texas Hold’em) against other players. Players will be able to play without having to leave their $Env Heaven-01 deposited anywhere other than their own wallet.",
         },
     },
     {
@@ -210,9 +210,9 @@ const RAW_TERMS = [
         TermKey: 'home-game-with-own-token-nfts-staking',
         TermValueByLanguage: {
             'pt-br':
-                'Jogo com token próprio, 2 coleções de NFTs e staking (e talvez PVP), baseado em um jogo antigo muito conhecido. A primeira rodada de venda do token será feita apenas em $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01.',
+                'Jogo com token próprio, 2 coleções de NFTs e staking (e talvez PVP), baseado em um jogo antigo muito conhecido. A primeira rodada de venda do token será feita apenas em $Env Heaven-01.',
             'en-us':
-                'Game with its own token, 2 NFT collections, and staking (and maybe PVP), based on a very well-known classic game. The first round of the token sale will be made only in $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01.',
+                'Game with its own token, 2 NFT collections, and staking (and maybe PVP), based on a very well-known classic game. The first round of the token sale will be made only in $Env Heaven-01.',
         },
     },
     {
@@ -234,8 +234,8 @@ const RAW_TERMS = [
     {
         TermKey: 'home-pichanha-coin-history',
         TermValueByLanguage: {
-            'pt-br': 'A história da Picanha Coin ($BASE-ANGULAR-PROJECT-PUBLIC-NAME-01)',
-            'en-us': 'The history of Picanha Coin ($BASE-ANGULAR-PROJECT-PUBLIC-NAME-01)',
+            'pt-br': 'A história da Picanha Coin ($Env Heaven-01)',
+            'en-us': 'The history of Picanha Coin ($Env Heaven-01)',
         },
     },
     {
@@ -249,9 +249,9 @@ const RAW_TERMS = [
         TermKey: 'home-brazil-2022-election-history',
         TermValueByLanguage: {
             'pt-br':
-                'No Brasil, as eleições presidenciais de 2022 foram muito marcantes por terem sido extremamente polarizadas. Durante o período de candidatura, “Lula”, um dos ex-presidentes, disse que, se fosse eleito, as pessoas pobres poderiam comer BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 (um corte nobre de boi).',
+                'No Brasil, as eleições presidenciais de 2022 foram muito marcantes por terem sido extremamente polarizadas. Durante o período de candidatura, “Lula”, um dos ex-presidentes, disse que, se fosse eleito, as pessoas pobres poderiam comer Env Heaven-01 (um corte nobre de boi).',
             'en-us':
-                'In Brazil, the 2022 presidential election was very memorable for being extremely polarized. During the candidacy period, “Lula,” one of the former presidents, said that if he were elected, poor people would be able to eat BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 (a prime cut of beef).',
+                'In Brazil, the 2022 presidential election was very memorable for being extremely polarized. During the candidacy period, “Lula,” one of the former presidents, said that if he were elected, poor people would be able to eat Env Heaven-01 (a prime cut of beef).',
         },
     },
     {
@@ -262,12 +262,12 @@ const RAW_TERMS = [
         },
     },
     {
-        TermKey: 'home-BASE-ANGULAR-PROJECT-01-de-lula-meme',
+        TermKey: 'home-jd-eh-ws-fe-01-de-lula-meme',
         TermValueByLanguage: {
             'pt-br':
-                'Mesmo hoje, muito tempo depois das eleições, a polarização política continua. E, desde então, a “BASE-ANGULAR-PROJECT-01 de Lula” foi — e continua sendo — um meme muito forte e reconhecido no Brasil, especialmente em discussões de política e em discursos de opositores.',
+                'Mesmo hoje, muito tempo depois das eleições, a polarização política continua. E, desde então, a “jd-eh-ws-fe-01 de Lula” foi — e continua sendo — um meme muito forte e reconhecido no Brasil, especialmente em discussões de política e em discursos de opositores.',
             'en-us':
-                'Even today, long after the elections, political polarization continues. Since then, “Lula’s BASE-ANGULAR-PROJECT-01” has been—and continues to be—a strong and well-known meme in Brazil, especially in political discussions and in opponents’ speeches.',
+                'Even today, long after the elections, political polarization continues. Since then, “Lula’s jd-eh-ws-fe-01” has been—and continues to be—a strong and well-known meme in Brazil, especially in political discussions and in opponents’ speeches.',
         },
     },
     {
@@ -278,19 +278,19 @@ const RAW_TERMS = [
         },
     },
     {
-        TermKey: 'home-BASE-ANGULAR-PROJECT-01-symbol-meaning',
+        TermKey: 'home-jd-eh-ws-fe-01-symbol-meaning',
         TermValueByLanguage: {
             'pt-br':
-                'O símbolo da BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 é $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01. Em português, “BASE-ANGULAR-PROJECT-01” é uma gíria para BASE-ANGULAR-PROJECT-PUBLIC-NAME-01.',
+                'O símbolo da Env Heaven-01 é $Env Heaven-01. Em português, “jd-eh-ws-fe-01” é uma gíria para Env Heaven-01.',
             'en-us':
-                'The symbol of BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 is $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01. In Portuguese, “BASE-ANGULAR-PROJECT-01” is slang for BASE-ANGULAR-PROJECT-PUBLIC-NAME-01.',
+                'The symbol of Env Heaven-01 is $Env Heaven-01. In Portuguese, “jd-eh-ws-fe-01” is slang for Env Heaven-01.',
         },
     },
     {
-        TermKey: 'home-avoid-hands-on-your-BASE-ANGULAR-PROJECT-01s',
+        TermKey: 'home-avoid-hands-on-your-jd-eh-ws-fe-01s',
         TermValueByLanguage: {
-            'pt-br': 'Evite que alguém passe a mão nas suas $BASE-ANGULAR-PROJECT-01s.',
-            'en-us': 'Avoid someone getting their hands on your $BASE-ANGULAR-PROJECT-01s.',
+            'pt-br': 'Evite que alguém passe a mão nas suas $jd-eh-ws-fe-01s.',
+            'en-us': 'Avoid someone getting their hands on your $jd-eh-ws-fe-01s.',
         },
     },
     {
@@ -310,10 +310,10 @@ const RAW_TERMS = [
         },
     },
     {
-        TermKey: 'home-BASE-ANGULAR-PROJECT-01',
+        TermKey: 'home-jd-eh-ws-fe-01',
         TermValueByLanguage: {
-            'pt-br': 'BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 ($BASE-ANGULAR-PROJECT-PUBLIC-NAME-01):',
-            'en-us': 'BASE-ANGULAR-PROJECT-PUBLIC-NAME-01 ($BASE-ANGULAR-PROJECT-PUBLIC-NAME-01):',
+            'pt-br': 'Env Heaven-01 ($Env Heaven-01):',
+            'en-us': 'Env Heaven-01 ($Env Heaven-01):',
         },
     },
 
@@ -335,10 +335,10 @@ const RAW_TERMS = [
         },
     },
     {
-        TermKey: 'home-who-conceived-BASE-ANGULAR-PROJECT-01-title',
+        TermKey: 'home-who-conceived-jd-eh-ws-fe-01-title',
         TermValueByLanguage: {
-            'pt-br': 'Quem idealizou a $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01',
-            'en-us': 'Who conceived $BASE-ANGULAR-PROJECT-PUBLIC-NAME-01',
+            'pt-br': 'Quem idealizou a $Env Heaven-01',
+            'en-us': 'Who conceived $Env Heaven-01',
         },
     },
     {
